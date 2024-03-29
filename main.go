9294ac70b2d5c1337a630cec5369255b3ba50b59
asvirts <96750426+asvirts@users.noreply.github.com>
2024-03-28 20:39:03 -0500

    variables and functions

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello")
+	whatWasSaid := say()
+	whatAge := age()
+	fmt.Println(whatWasSaid, whatAge)
+}
+
+func say() string {
+	return "Hello, I am"
+}
+
+func age() int {
+	return 33
 }
